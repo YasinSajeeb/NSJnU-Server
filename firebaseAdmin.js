1077f24f86b053d47475867f6efb601f389ef976
YasinSajeeb <yeasinalhossen@gmail.com>
2024-08-20 22:08:15 +0600

    new commit

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require('./nsjnu-d8a4e-firebase-adminsdk-q55bi-cdebfb948d.json'); 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
 });
 
 module.exports = admin;
